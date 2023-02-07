9f0449ebb06c26f21213e8a190dd288a312b40ed
Priller Patrick <prillerpatrick@gmail.com>
2023-02-07 19:33:30 +0100

    created MainForm.cs

---
+++ AirBattery/MainForm.cs
@@ -0,0 +1,16 @@
+using System.Windows.Forms;
+
+namespace AirBattery;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+        // retrieve the battery percentage of the AirPods
+    }
+}
